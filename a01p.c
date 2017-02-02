6b46b06db955e64854a6b0ef325913e03ef94a47
DavidJHartman <djhartman1216@gmail.com>
2017-02-01 23:29:48 -0800

    The endian and signbit checker works

--- a01p.c
+++ a01p.c
@@ -5,7 +5,7 @@
 //      * printNumberData()
 //   * change studentName by changing "I. Forgot" to your actual name
 
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
 #include <stdbool.h>
 
@@ -14,13 +14,17 @@ static char *studentName = "David Hartman";
 // report whether machine is big or small endian
 void bigOrSmallEndian()
 {
-    short i = 0xAABB;
-    char* p;
-    p = &i; 
-    if (p[0]>p[1])
-        printf("Your system is little-endian");
+    printf("byte order: ");
+
+    unsigned short i = (short)0xAABB;
+    char*          p;
+
+    p = (char*)&i;
+
+    if (p[0]>p[1]) 
+        printf("little-endian\n");
     else
-        printf("your system is big-endian");
+        printf("big-endian\n");
 }
 
 // get next int (entered in hex) using scanf()
@@ -29,13 +33,16 @@ void bigOrSmallEndian()
 bool getNextHexInt(unsigned int *iPtr)
 {
 	// replace this code with the call to scanf()
-	*iPtr = 0;
-	return true;
+	return scanf("%X",iPtr);
 }
 
 // print requested data for the given number
 void printNumberData(int i)
 {
+    int signBitMask = 1 << ((sizeof(int)*8-1));
+    int expBitMask  = 
+    printf("signBit: %i, ",i&signBitMask ? 1 : 0);
+
 }
 
 // do not change this function in any way
