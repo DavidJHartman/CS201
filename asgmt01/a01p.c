ef755863392637b110b872402b7a8659c7775922
DavidJHartman <djhartman1216@gmail.com>
2017-02-05 15:54:42 -0800

    Finished assignment one, no warnings, no errors

--- asgmt01/a01p.c
+++ asgmt01/a01p.c
@@ -39,10 +39,56 @@ bool getNextHexInt(unsigned int *iPtr)
 // print requested data for the given number
 void printNumberData(int i)
 {
-    int signBitMask = 1 << ((sizeof(int)*8-1));
-    int expBitMask  = 
-    printf("signBit: %i, ",i&signBitMask ? 1 : 0);
+    int signBitMask;
+    int fracBitMask;
+    int expBitMask;
+    int signBit;
+    int expBits;
+    int fracBits;
+    int bias;
+    signBitMask = 1 << ((sizeof(int)*8-1));
+    expBitMask = signBitMask >> 8;
+    expBitMask ^=signBitMask;
+    fracBitMask= 0x7FFFFFFF >> 9;
+    signBit = i&signBitMask ? 1 : 0;
+    expBits = (i&expBitMask)>>23;
+    fracBits = i&fracBitMask;
+    bias = 2;
+    for ( int i = 0; i < 6; i++ ) {
+        bias *= 2;
+    }
+    bias -= 1;
+    printf("signBit:\t%d, ", signBit);
+    printf("expBits:\t%d, ", expBits);
+    printf("fracBits: \t0x%.8X\n", fracBits);
 
+    if ( expBits == 255 ) { 
+       if ( fracBits == 0 ) {
+            if ( signBit == 0)
+                printf("+infinity\n");
+            else
+                printf("-infinity\n");
+       }
+       else {
+            if ( fracBits >= 400000) 
+                printf("QNaN\n");
+            else 
+                printf("SNaN\n");
+       }
+    }
+    else if ( expBits == 0 ) {
+        if ( fracBits == 0 ) {
+            if ( signBit == 0 )
+                printf("+zero\n");
+            else 
+                printf("-zero\n");
+        }
+        else
+            printf("denormalized: \texp: %d\n", 1 - bias);
+    }
+    else 
+        printf("normalized: \texp: %d\n", expBits-bias);
+    printf("\n");
 }
 
 // do not change this function in any way
@@ -57,8 +103,8 @@ int main(int argc, char **argv)
 		if (argc == 1)						// allow grading script to control ...
 			printf("> ");					// ... whether prompt character is printed
 		validInput = getNextHexInt(&i);
-		printf("0x%08X\n", i);
-		if (! validInput) {					// encountered bad input
+		printf("0x%08X\n", i); 
+        if (! validInput) {					// encountered bad input
 			printf("bad input\n");
 			while (getchar() != '\n') ;		// flush bad line from input buffer
 			continue;
